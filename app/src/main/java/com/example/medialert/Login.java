895087a11605a1da8aa03b2a6ac642ed2ab08213
MASHIAT TABASSUM KHAN <125111911+Mashiat12@users.noreply.github.com>
2023-11-03 03:34:33 +0600

    login

--- app/src/main/java/com/example/medialert/Login.java
+++ app/src/main/java/com/example/medialert/Login.java
@@ -1,21 +1,23 @@
 package com.example.medialert;
- import androidx.annotation.NonNull;
- import androidx.appcompat.app.AppCompatActivity;
- import android.content.Intent;
- import android.os.Bundle;
- import android.text.TextUtils;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
- import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
     TextInputEditText editTextEmail, editTextPassword;
@@ -71,12 +73,12 @@ public class Login extends AppCompatActivity {
 
                     return;
                 }
-                mAuth.SignInWithEmailAndPassword(email, password)
+                mAuth.signInWithEmailAndPassword(email, password)
                         .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                ProgressBar.setVisibility(View.GONE);
-                            }
+                                progressBar.setVisibility(View.GONE);
+
                                 if (task.isSuccessful()) {
                                     Toast.makeText(Login.this, "login successful.",
                                             Toast.LENGTH_SHORT).show();
@@ -94,4 +96,6 @@ public class Login extends AppCompatActivity {
                         });
 
             }
+        });
+    }
 }
