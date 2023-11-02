0a4ac9608024ce56069167f1725dabee69912831
MASHIAT TABASSUM KHAN <125111911+Mashiat12@users.noreply.github.com>
2023-11-03 03:00:20 +0600

    registration

--- app/src/main/java/com/example/medialert/MainActivity.java
+++ app/src/main/java/com/example/medialert/MainActivity.java
@@ -5,19 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
-
-
+    FirebaseAuth auth;
+    Button button;
+    textView textView;
+    FirebaseUser user ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
     }
 }
