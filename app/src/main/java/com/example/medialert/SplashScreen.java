0f4ef2fdce02b095029f6c801a6956cf60bac0ca
MASHIAT TABASSUM KHAN <125111911+Mashiat12@users.noreply.github.com>
2023-11-22 20:17:22 +0600

    space

--- app/src/main/java/com/example/medialert/SplashScreen.java
+++ app/src/main/java/com/example/medialert/SplashScreen.java
@@ -1,7 +1,5 @@
 package com.example.medialert;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,37 +10,27 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import java.io.File;
-
 
 public class SplashScreen extends AppCompatActivity {
 
     ImageView splashLogo;
     TextView Slogan;
-
-
     Animation topAnim,bottomAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
         //hooks
         splashLogo = findViewById(R.id.splash_logo);
         Slogan =findViewById(R.id.slogan);
-
 
         topAnim = AnimationUtils.loadAnimation(this, R.anim.top_anim);
         bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
         //set animation
         splashLogo.setAnimation(topAnim);
         Slogan.setAnimation(bottomAnim);
-
-
-
-
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
