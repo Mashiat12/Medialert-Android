9d1a86f374c764913acff1bea1358ab914c322dd
Maria Akter Abin <b190305040@cse.jnu.ac.bd>
2023-11-28 18:17:21 +0600

    fix bugs(Dashboard,health,add)

--- app/src/main/java/com/example/medialert/HomeActivity.java
+++ app/src/main/java/com/example/medialert/HomeActivity.java
@@ -1,29 +1,76 @@
 package com.example.medialert;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
+import android.widget.GridLayout;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity  {
+    GridLayout mainGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mainGrid = (GridLayout) findViewById(R.id.mainGrid);
+        setSingleEvent(mainGrid);
+    }
 
-        Intent ihome = new Intent(HomeActivity.this, MainActivity.class);
+    private void setSingleEvent(GridLayout mainGrid) {
+        for(int i=0;i<mainGrid.getChildCount();i++){
+            CardView cardView = (CardView) mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (finalI == 0) {
 
-        Handler handler=new Handler();
-        Runnable runnable=new Runnable() {
-            @Override
-            public void run() {
-                startActivity(ihome);
-
-            }
-        };
+                        Intent intent = new Intent(HomeActivity.this, labtest.class);
+                        startActivity(intent);
 
 
+                    }
+                    if (finalI == 1) {
+
+                        Intent intent = new Intent(HomeActivity.this, health.class);
+                        startActivity(intent);
+
+
+                    }
+                    if (finalI == 2) {
+
+                        Intent intent = new Intent(HomeActivity.this, addmedicin.class);
+                        startActivity(intent);
+
+
+                    }
+                    if (finalI == 3) {
+
+                        Intent intent = new Intent(HomeActivity.this,buymedicin.class);
+                        startActivity(intent);
+
+
+                    }
+                    if (finalI == 4) {
+
+                        Intent intent = new Intent(HomeActivity.this, scanner.class);
+                        startActivity(intent);
+
+
+                    }
+                    if (finalI == 5) {
+
+                        Intent intent = new Intent(HomeActivity.this, logoutt.class);
+                        startActivity(intent);
+
+
+                    }
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/example/medialert/SplashScreen.java
+++ app/src/main/java/com/example/medialert/SplashScreen.java
@@ -1,5 +1,7 @@
 package com.example.medialert;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,29 +13,26 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 import java.io.File;
+
 
 public class SplashScreen extends AppCompatActivity {
 
     ImageView splashLogo;
     TextView Slogan;
+
+
     Animation topAnim,bottomAnim;
-
-
-    private FirebaseAuth mAuth ;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
         //hooks
         splashLogo = findViewById(R.id.splash_logo);
         Slogan =findViewById(R.id.slogan);
+
 
         topAnim = AnimationUtils.loadAnimation(this, R.anim.top_anim);
         bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
@@ -41,29 +40,16 @@ public class SplashScreen extends AppCompatActivity {
         splashLogo.setAnimation(topAnim);
         Slogan.setAnimation(bottomAnim);
 
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
 
 
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
-                    Log.d("myTag", "This is from splash screen");
-
-
-                FirebaseUser currentUser = mAuth.getCurrentUser();
-                if(currentUser != null){
-                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                }else {
-                    Intent intent = new Intent(SplashScreen.this, SignIn.class);
-                    startActivity(intent);
-                    finish();//finishing it because splash screen should be shown only once
-                }
+                final Intent mainIntent = new Intent(SplashScreen.this,HomeActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
             }
-        }, 1500);//for timer
+        }, 5000);
     }
     }
